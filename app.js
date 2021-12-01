b837845493c60ec92df0a2acc91adecab8b6a678
Hanna <hanna.milk@khk.ee>
2021-12-01 10:25:04 +0200

    Added task deleting from local storage function

--- app.js
+++ app.js
@@ -23,10 +23,26 @@ function delTask(event){
     if(event.target.textContent === "X"){
         if(confirm("Do you want to delete this task?")){
             event.target.parentElement.remove()
+            //delete tasks from storage
+            let task = event.target.parentElement.textContent.slice(0, -1)
+            delTaskStorage(task)
         }
     }
 }
-
+function delTaskStorage(task) {
+    let tasks
+    if(localStorage.getItem("tasks")=== null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"))
+    }
+    tasks.forEach(function (taskFromLS, taskIndex){
+        if(taskFromLS === task){
+            tasks.splice(taskIndex, 1)
+        }
+    })
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
 
 // add task function
 function addTask(event){
